149855e6f340494039c04cc6df37b7877516a964
Bruna Reveriego <94184681+BrunaReveriego91@users.noreply.github.com>
2024-01-16 17:26:01 -0300

    adicionando ngFor

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos = [
+    {
+      conteudo: 'Comunicação entre componentes',
+      autoria: 'Bruna Reveriego',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo1',
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com o @Input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2',
+    }
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
