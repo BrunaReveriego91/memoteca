b56a823123995dd0128c0516e9cc34b9e902ce91
Bruna Reveriego <94184681+BrunaReveriego91@users.noreply.github.com>
2024-01-17 09:57:38 -0300

    adicionando o ngIf e else

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,21 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
   listaPensamentos = [
-    {
-      conteudo: 'Comunicação entre componentes',
-      autoria: 'Bruna Reveriego',
-      modelo: 'modelo3',
-    },
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente pai',
-      modelo: 'modelo1',
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com o @Input()',
-      autoria: 'Componente filho',
-      modelo: 'modelo2',
-    }
+    // {
+    //   conteudo: 'Comunicação entre componentes',
+    //   autoria: 'Bruna Reveriego',
+    //   modelo: 'modelo3',
+    // },
+    // {
+    //   conteudo: 'Passo informações para o componente filho',
+    //   autoria: 'Componente pai',
+    //   modelo: 'modelo1',
+    // },
+    // {
+    //   conteudo: 'Minha propriedade é decorada com o @Input()',
+    //   autoria: 'Componente filho',
+    //   modelo: 'modelo2',
+    // }
   ];
   constructor() {}
 
